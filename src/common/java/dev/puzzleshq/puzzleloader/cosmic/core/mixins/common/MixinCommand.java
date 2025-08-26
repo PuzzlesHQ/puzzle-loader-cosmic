4438f5f3ceefe8b3cadd8beb6dd781b7135c965f
Mr-Zombii <thezombiigames@gmail.com>
2025-08-25 20:04:01 -0500

    added MixinZoneGenerator to mixins.json, added EventRegisterCommand

---
+++ src/common/java/dev/puzzleshq/puzzleloader/cosmic/core/mixins/common/MixinCommand.java
@@ -0,0 +1,41 @@
+package dev.puzzleshq.puzzleloader.cosmic.core.mixins.common;
+
+import com.llamalad7.mixinextras.lib.apache.commons.tuple.Pair;
+import dev.puzzleshq.puzzleloader.cosmic.game.GameRegistries;
+import dev.puzzleshq.puzzleloader.cosmic.game.events.command.EventRegisterCommand;
+import finalforeach.cosmicreach.chat.commands.Command;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+@Mixin(Command.class)
+public class MixinCommand {
+
+    @Unique
+    private final static String[] puzzle_loader_cosmic$empty = new String[0];
+
+    @Inject(method = "<clinit>", at = @At("TAIL"))
+    private static void clinit() {
+        List<Pair<String[], Supplier<Command>>> commandFactories = new ArrayList<>();
+
+        EventRegisterCommand commandEvent = new EventRegisterCommand(commandFactories);
+        GameRegistries.COSMIC_EVENT_BUS.post(commandEvent);
+
+        for (Pair<String[], Supplier<Command>> commandFactory : commandFactories) {
+            String[] aliases = commandFactory.getKey();
+            if (aliases.length > 1) {
+                String[] knockoffs = Arrays.copyOfRange(aliases, 1, aliases.length);
+                Command.registerCommand(commandFactory.getRight(), aliases[0], knockoffs);
+                continue;
+            }
+            Command.registerCommand(commandFactory.getRight(), aliases[0], puzzle_loader_cosmic$empty);
+        }
+    }
+
+}
---
+++ src/common/java/dev/puzzleshq/puzzleloader/cosmic/game/events/command/EventRegisterCommand.java
@@ -0,0 +1,24 @@
+package dev.puzzleshq.puzzleloader.cosmic.game.events.command;
+
+import com.llamalad7.mixinextras.lib.apache.commons.tuple.ImmutablePair;
+import com.llamalad7.mixinextras.lib.apache.commons.tuple.Pair;
+import finalforeach.cosmicreach.chat.commands.Command;
+import net.neoforged.bus.api.Event;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class EventRegisterCommand extends Event {
+
+    private final List<Pair<String[], Supplier<Command>>> commandFactories;
+
+    public EventRegisterCommand(List<Pair<String[], Supplier<Command>>> commandFactories) {
+        this.commandFactories = commandFactories;
+    }
+
+    public void register(Supplier<Command> commandSupplier, String... aliases) {
+        this.commandFactories.add(new ImmutablePair<>(aliases, commandSupplier));
+    }
+
+}
